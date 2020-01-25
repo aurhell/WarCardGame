727e7033326f1389cb3a6cf8533497d410cb3cb5
Aurélien Girault <me@agirault.com>
2020-01-25 16:51:16 +0100

    add logic

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var cpuScoreLabel: UILabel!
+    
+    var playerScore:Int = 0
+    var cpuScore: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber:Int = Int.random(in: 2...14)
+        let rightNumber:Int = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+        
+        if (leftNumber > rightNumber) {
+            playerScore += 1
+            playerScoreLabel.text = String(playerScore)
+        }
+        
+        if (rightNumber > leftNumber) {
+            cpuScore += 1
+            cpuScoreLabel.text = String(cpuScore)
+        }
+    }
 }
 
