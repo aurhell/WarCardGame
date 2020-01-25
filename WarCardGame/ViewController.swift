38e9af8f9ae547bec73d4d4956d41fb27047785c
Aurélien Girault <me@agirault.com>
2020-01-25 11:39:10 +0100

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Aurélien Girault on 25/01/2020.
+//  Copyright © 2020 Aurélien Girault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
